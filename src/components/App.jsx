65d5b996aeed4a494d94dada078766476255c1b4
NikitaSimakov <mishaaniskin@gmail.com>
2023-05-29 11:25:10 +0300

    Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+
+import Home from "pages/home";
+import Movies from "pages/movies";
+
+import { Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="*" element={<h1>Page not found. Error 404</h1>}/>
+      </Routes>
     </div>
   );
 };
+  
